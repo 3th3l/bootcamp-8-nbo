2cd167a40abb4bdac439fe215e1139ff7975ea62
Ethel Awino <awino.ethel@gmail.com>
2016-06-17 11:46:52 +0300

    Changes made to fizzbuzz.py and FizzBuzz.py deleted

--- day2/fizzbuzz.py
+++ day2/fizzbuzz.py
@@ -1,9 +1,15 @@
-def fizz_buzz():
+def fizz_buzz(n):
+    
 	""" return fizz when divisible by 3
 	return buzz when n is divisible by 5
 	return fizzbuzz when n is divisible by both 3 and 5
 	"""
-	if n % 3 ==0:
-		return 'fizz'
-	else:
-		return 'buzz'
+
+	if n % 15 == 0:
+        return 'FizzBuzz'
+	elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+    	return 'Buzz'
+    else:
+        return n
